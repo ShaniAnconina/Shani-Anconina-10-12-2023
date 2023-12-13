4277c73956361264a9c05518522cb03a80f26f59
Shani Anconina <shanianconina310@gmail.com>
2023-12-13 15:07:39 +0200

    Home page template

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,20 +3,46 @@ export function HomePage() {
 
   return (
     <div className="home-page">
-      <input type="search" placeholder="Type a city name" />
+      <input className="search-location" type="search" placeholder="Type a location" />
       <div className="location-weather">
         <div className="top-section">
-          <div className="left">
-            <img src={require("../assets/img/rain-cloud.png")} className="weather-img" />
+          <div className="curr-weather">
+            <img className="weather-icon" src={require("../assets/img/rain-cloud.png")} />
             <div>
               <p className="location-name">Tel Aviv</p>
               <p className="location-degrees">38°</p>
             </div>
           </div>
-          <div className="right"></div>
+          <div className="add-to-favorites">favorites</div>
         </div>
-        <div className="weather-title">Sunny</div>
-        <div className="top-section"></div>
+        <h1 className="weather-title">Sunny</h1>
+        <div className="weekly-weather">
+          <div className="daily-weather">
+            <img className="weather-icon" src={require("../assets/img/rain-cloud.png")} />
+            <p>Sun</p>
+            <span className="daily-degrees">38°</span>
+          </div>
+          <div className="daily-weather">
+            <img className="weather-icon" src={require("../assets/img/rain-cloud.png")} />
+            <p>Mon</p>
+            <span className="daily-degrees">38°</span>
+          </div>
+          <div className="daily-weather">
+            <img className="weather-icon" src={require("../assets/img/rain-cloud.png")} />
+            <p>Tue</p>
+            <span className="daily-degrees">38°</span>
+          </div>
+          <div className="daily-weather">
+            <img className="weather-icon" src={require("../assets/img/rain-cloud.png")} />
+            <p>Wed</p>
+            <span className="daily-degrees">38°</span>
+          </div>
+          <div className="daily-weather">
+            <img className="weather-icon" src={require("../assets/img/rain-cloud.png")} />
+            <p>Thu</p>
+            <span className="daily-degrees">38°</span>
+          </div>
+        </div>
       </div>
     </div>
   );
